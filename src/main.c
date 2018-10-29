825521906ab2f02805625d65bdfa186dbed0a684
rso16 <rick.overhorst@gmail.com>
2018-10-29 01:15:35 +0100

    testing cursor movement

--- src/main.c
+++ src/main.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+#define gotoxy(x,y) printf("\033[%d;%dH", (x), (y))
 
 char cmd1[5] = {0x1B, '[', '2', 'J', '\0'}; // Clear screen
-char cmd2[4] = {0x1B, '[', 'f', '\0'}; // Cursor home
+char cmd2[4] = {0x1B, '[', 'h', '\0'}; // Cursor home
+char cmd3[] = {0x1B, '[',  '1', '0','0', ';', '1', '0', '0', 'h', '\0'}; // Cursor home
 
 int main()
 {
-    printf(cmd1);
-    printf(cmd2);
-
-
-
+    int c;
+    printf("%s", cmd1);
+    gotoxy(100,200);
+    printf("hello world\n");    
+    scanf("%c", &c);
+    printf("%s", cmd1);
+//    while(1);
 
     return 0;
 }
