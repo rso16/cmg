8749a475785313c3d5bf0672e87a954357f77950
rso16 <rick.overhorst@gmail.com>
2018-11-04 22:43:26 +0100

    first commit

--- makefile
+++ makefile
@@ -2,7 +2,7 @@ OBJS = bin/main.o
 CC = gcc
 CFLAGS = -Wall -c
 LFLAGS = -Wall
-NAME = bin/tui_test
+NAME = bin/xmake
 
 
 all: $(OBJS)
--- src/main.c
+++ src/main.c
@@ -1,20 +1,22 @@
+//made by RSO16
+//includes
 #include <stdio.h>
+#include <limits.h>
+#include <unistd.h>
+
+//defines
 #define gotoxy(x,y) printf("\033[%d;%dH", (x), (y))
 
 char cmd1[5] = {0x1B, '[', '2', 'J', '\0'}; // Clear screen
-char cmd2[4] = {0x1B, '[', 'h', '\0'}; // Cursor home
-char cmd3[] = {0x1B, '[',  '1', '0','0', ';', '1', '0', '0', 'h', '\0'}; // Cursor home
+char cmd2[4] = {0x1B, '[', 'f', '\0'}; // Cursor home
 
-int main()
+int main(int argc, char **argv)
 {
-    int c;
-    printf("%s", cmd1);
-    gotoxy(100,200);
-    printf("hello world\n");    
-    scanf("%c", &c);
-    printf("%s", cmd1);
-//    while(1);
+	char cwd[PATH_MAX];
+	getcwd(cwd, sizeof(cwd));
+	printf("cwd = %s\n", cwd);
+	
 
-    return 0;
+	return 0;
 }
 
